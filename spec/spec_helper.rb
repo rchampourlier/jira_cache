8ef765453684362063953db03a89b70d13ec48c5
Romain Champourlier <pro@rchampourlier.com>
2015-06-19 01:12:57 +0200

    Updated Travis config, added Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../..', __FILE__)
 require 'config/boot'
+
+require 'coveralls'
+Coveralls.wear!
+
 require 'lib/jira_cache'
 
 # Cleaning database after each test
