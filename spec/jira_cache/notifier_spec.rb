4a46a63a0b950fc9d483716e35a96627b64d7919
Romain Champourlier <pro@rchampourlier.com>
2015-07-07 09:05:45 +0200

    Fixed notifier_spec

--- spec/jira_cache/notifier_spec.rb
+++ spec/jira_cache/notifier_spec.rb
@@ -5,13 +5,14 @@ describe JiraCache::Notifier do
 
   describe '::published(name, data = nil)' do
     let(:event_name) { 'test' }
-    let(:data) { { data: 1 } }
+    let(:issue_data) { { 'value' => 'issue_value', 'key' => 'issue_data_key' } }
+    let(:event_data) { { key: 'issue_key', data: issue_data } }
 
-    it 'logs the event name and data' do
+    it 'logs the event name and event\'s data key' do
       logger = ::Logger.new('/dev/null')
       notifier = described_class.new(logger)
-      expect(logger).to receive(:info).with("[test] #{data}")
-      notifier.publish event_name, data
+      expect(logger).to receive(:info).with("[test] #{event_data[:key]}")
+      notifier.publish event_name, event_data
     end
   end
 end
