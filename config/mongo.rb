fea5ab6f5cbc1d6c53a4cf15d837410e5533a511
Romain Champourlier <pro@rchampourlier.com>
2016-11-20 15:05:10 +0100

    [postgresql] removed mongo stuff

--- config/mongo.rb
+++
@@ -1,8 +0,0 @@
-require 'mongoid'
-
-env = ENV['JIRA_CACHE_ENV']
-fail 'JIRA_CACHE_ENV environment variable must be set' if env.nil?
-
-ENV['MONGOID_ENV'] = env
-
-Mongoid.load! File.expand_path('../mongoid.yml', __FILE__)
